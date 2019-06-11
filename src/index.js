6eea4311d8ccca70a38722e4f3fd0b9480060da5
Raphael Lopes <ph.campanario07@gmail.com>
2019-06-11 14:47:17 -0300

    Add mongoose and configure it to the server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const server = require('./server');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 server.listen(port, () => (
   console.log(`Listening on http://localhost:${port}`)
--- src/server.js
+++ src/server.js
@@ -2,25 +2,35 @@ require('dotenv').config();
 
 // vendors
 const express = require('express');
+const mongoose = require('mongoose');
 
 // locals
 const routes = require('./routes');
+const databaseConfig = require('./config/database');
 
 class App {
   constructor() {
     this.express = express();
 
+    this.database();
     this.routes();
 
     this.middlewares();
   }
 
-  middlewares() {
-    this.express.use(express.json());
+  database() {
+    this.mongoose = mongoose.connect(databaseConfig.uri, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+    });
   }
 
   routes() {
     this.express.use(routes);
+  }
+
+  middlewares() {
+    this.express.use(express.json());
   }
 }
 
